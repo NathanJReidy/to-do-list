8e04618ef55d7c7f068aef7395be87075bf720d6
Nathan Reidy <nathanjreidy@gmail.com>
2020-07-08 16:04:51 +1000

    added submit function to editProject

--- src/DOMfunctions.js
+++ src/DOMfunctions.js
@@ -14,10 +14,25 @@ const newProject = (() => {
 const editProject = (() => {
     const editBtn = document.querySelector('.editdiv');
     const projectInfo = document.querySelector('#nameDescEdit');
-    
+
     const editProjectForm = document.querySelector('#editProjectForm');
     const name = document.querySelector('.pnameform');
     const description = document.querySelector('.pdescform');
     const submitBtn = document.querySelector('.formsubmit');
     const cancelBtn = document.querySelector('.formcancel');
+
+    function show() {
+        projectInfo.style.display = 'none';
+        editProjectForm.style.display = 'block';
+    }
+
+    function hide() {
+        projectInfo.style.display = 'block';
+        editProjectForm.style.display = 'none';
+    }
+
+    function submit() {
+        updateProject(name.value, description.value);
+        // Need to make a function that updates the project name and project description shown on the screen based on the inputted submitted values.
+    }
 })
