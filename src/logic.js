7a28f50d34fca5bcac4951604722c9ff5edee7bb
Nathan Reidy <nathanjreidy@gmail.com>
2020-07-09 20:19:21 +1000

    Updated logic.js with createTask function

---
+++ src/logic.js
@@ -0,0 +1,21 @@
+import {allProjects} from './index'
+
+const createProject = (name, description) => {
+    allProjects.push({
+        name,
+        description,
+        toDoListItems: [],
+        active: true,
+    })
+}
+
+const createTask = (name, date) => {
+    let activeProject = allProjects.filter(project => project.active === true);
+    let toDoArray = activeProject[0].toDoListItems;
+    toDoArray.push({
+        name,
+        date,
+        active: false,
+        complete: false,
+    })
+}
